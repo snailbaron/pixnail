40cd2b74c1ea92506878ff9dbdc1ed4b7dc6075c
snailbaron <murbidodrus@gmail.com>
2017-03-30 01:37:54 +0300

    Initial state
    * Basic main window layout
    * Content dialog for color picking with working controls
    
    Able to open a file and zoom in/out.

---
+++ upixnail/ColorState.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Media;
+
+namespace upixnail {
+    class ColorState : INotifyPropertyChanged {
+        public event PropertyChangedEventHandler PropertyChanged = delegate {};
+
+        class RGBComponent {
+            public byte R;
+            public byte G;
+            public byte B;
+
+            public RGBComponent(byte r, byte g, byte b)
+            {
+                R = r;
+                G = g;
+                B = b;
+            }
+        }
+
+        class HSVComponent {
+            public ushort H;
+            public byte S;
+            public byte V;
+
+            public HSVComponent(ushort h, byte s, byte v)
+            {
+                H = h;
+                S = s;
+                V = v;
+            }
+        }
+
+        RGBComponent _rgb = new RGBComponent(0, 0, 0);
+        HSVComponent _hsv = new HSVComponent(0, 0, 0);
+
+        // RGB properties
+        public byte RGB_R {
+            get { return _rgb.R; }
+            set {
+                if (_rgb.R != value) {
+                    _rgb.R = value;
+                    UpdateFromRGB();
+                }
+            }
+        }
+        public byte RGB_G {
+            get { return _rgb.G; }
+            set {
+                if (_rgb.G != value) {
+                    _rgb.G = value;
+                    UpdateFromRGB();
+                }
+            }
+        }
+        public byte RGB_B {
+            get { return _rgb.B; }
+            set {
+                if (_rgb.B != value) {
+                    _rgb.B = value;
+                    UpdateFromRGB();
+                }
+            }
+        }
+
+        // HSV properties
+        public ushort HSV_H {
+            get { return _hsv.H; }
+            set {
+                if (_hsv.H != value) {
+                    _hsv.H = value;
+                    UpdateFromHSV();
+                }
+            }
+        }
+        public byte HSV_S {
+            get { return _hsv.S; }
+            set {
+                if (_hsv.S != value) {
+                    _hsv.S = value;
+                    UpdateFromHSV();
+                }
+            }
+        }
+        public byte HSV_V {
+            get { return _hsv.V; }
+            set {
+                if (_hsv.V != value) {
+                    _hsv.V = value;
+                    UpdateFromHSV();
+                }
+            }
+        }
+
+        public SolidColorBrush ColorBrush {
+            get { return new SolidColorBrush(Color.FromArgb(255, _rgb.R, _rgb.G, _rgb.B)); }
+        }
+
+        void AllPropsChanged()
+        {
+        }
+
+        void UpdateFromRGB()
+        {
+            Convert(_rgb, _hsv);
+            PropertyChanged(this, new PropertyChangedEventArgs("HSV_H"));
+            PropertyChanged(this, new PropertyChangedEventArgs("HSV_S"));
+            PropertyChanged(this, new PropertyChangedEventArgs("HSV_V"));
+            PropertyChanged(this, new PropertyChangedEventArgs("ColorBrush"));
+            //AllPropsChanged();
+        }
+
+        void UpdateFromHSV()
+        {
+            Convert(_hsv, _rgb);
+            PropertyChanged(this, new PropertyChangedEventArgs("RGB_R"));
+            PropertyChanged(this, new PropertyChangedEventArgs("RGB_G"));
+            PropertyChanged(this, new PropertyChangedEventArgs("RGB_B"));
+            PropertyChanged(this, new PropertyChangedEventArgs("ColorBrush"));
+            //AllPropsChanged();
+        }
+
+        void Convert(HSVComponent hsv, RGBComponent rgb)
+        {
+            double rs = hsv.S / 100.0;
+            double rv = hsv.V / 100.0;
+
+            double c = rv * rs;
+
+
+            double x = c * (1 - Math.Abs(((hsv.H / 60.0) % 2) - 1));
+            double m = rv - c;
+
+            Tuple<double, double, double> rrgb;
+            if (hsv.H < 60) {
+                rrgb = new Tuple<double, double, double>(c, x, 0);
+            } else if (hsv.H < 120) {
+                rrgb = new Tuple<double, double, double>(x, c, 0);
+            } else if (hsv.H < 180) {
+                rrgb = new Tuple<double, double, double>(0, c, x);
+            } else if (hsv.H < 240) {
+                rrgb = new Tuple<double, double, double>(0, x, c);
+            } else if (hsv.H < 300) {
+                rrgb = new Tuple<double, double, double>(x, 0, c);
+            } else {
+                rrgb = new Tuple<double, double, double>(c, 0, x);
+            }
+
+            rgb.R = (byte)((rrgb.Item1 + m) * 255.0);
+            rgb.G = (byte)((rrgb.Item2 + m) * 255.0);
+            rgb.B = (byte)((rrgb.Item3 + m) * 255.0);
+        }
+
+        void Convert(RGBComponent rgb, HSVComponent hsv)
+        {
+            double rr = rgb.R / 255.0;
+            double rg = rgb.G / 255.0;
+            double rb = rgb.B / 255.0;
+
+            double cmax = Math.Max(rr, Math.Max(rg, rb));
+            double cmin = Math.Min(rr, Math.Min(rg, rb));
+            double delta = cmax - cmin;
+
+            if (delta == 0.0) {
+                hsv.H = 0;
+            } else if (cmax == rr) {
+                hsv.H = (ushort)(60 * ((int)((rg - rb) / delta) % 6));
+            } else if (cmax == rg) {
+                hsv.H = (ushort)(60 * ((rb - rr) / delta + 2));
+            } else {
+                hsv.H = (ushort)(60 * ((rr - rg) / delta + 4));
+            }
+
+            if (cmax == 0.0) {
+                hsv.S = 0;
+            } else {
+                hsv.S = (byte)((delta / cmax) * 100.0);
+            }
+
+            hsv.V = (byte)(cmax * 100.0);
+        }
+    }
+}
---
+++ upixnail/MainPage.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.Effects;
+using Microsoft.Graphics.Canvas.UI.Xaml;
+using Windows.Storage.Streams;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace upixnail {
+    public sealed partial class MainPage : Page {
+        const float DefaultZoomFactor = 1.5f;
+
+        CanvasVirtualBitmap _bitmap = null;
+        ScaleEffect _scaleEffect = new ScaleEffect();
+
+        public MainPage()
+        {
+            InitializeComponent();
+            _scaleEffect.InterpolationMode = CanvasImageInterpolation.NearestNeighbor;
+        }
+
+        private async void MenuOpen_Click(object sender, RoutedEventArgs e)
+        {
+            var picker = new FileOpenPicker();
+            picker.FileTypeFilter.Add(".png");
+            StorageFile file = await picker.PickSingleFileAsync();
+            if (file != null) {
+                using (var stream = await file.OpenReadAsync()) {
+                    _bitmap = await CanvasVirtualBitmap.LoadAsync(
+                        ImageCanvas,
+                        stream,
+                        CanvasVirtualBitmapOptions.CacheOnDemand);
+                }
+                _scaleEffect.Source = _bitmap;
+                _scaleEffect.Scale = new Vector2(1, 1);
+                ImageCanvas.Width = _bitmap.Size.Width;
+                ImageCanvas.Height = _bitmap.Size.Height;
+                ImageCanvas.Invalidate();
+            }
+        }
+
+        private void ImageCanvas_Draw(
+            CanvasControl sender,
+            CanvasDrawEventArgs args)
+        {
+            CanvasDrawingSession session = args.DrawingSession;
+            if (_bitmap != null) {
+                session.DrawImage(_scaleEffect);
+            }
+        }
+
+        private void Page_Unloaded(object sender, RoutedEventArgs e)
+        {
+            ImageCanvas.RemoveFromVisualTree();
+            ImageCanvas = null;
+        }
+
+        void ZoomUpdated()
+        {
+            ImageCanvas.Width = _bitmap.Size.Width * _scaleEffect.Scale.X;
+            ImageCanvas.Height = _bitmap.Size.Height * _scaleEffect.Scale.Y;
+            ZoomText.Text = String.Format("{0} %", (uint)(_scaleEffect.Scale.X * 100));
+        }
+
+        private void ZoomIn_Click(object sender, RoutedEventArgs e)
+        {
+            _scaleEffect.Scale = Vector2.Multiply(_scaleEffect.Scale, DefaultZoomFactor);
+            ZoomUpdated();
+            ImageCanvas.Invalidate();
+        }
+
+        private void ZoomOut_Click(object sender, RoutedEventArgs e)
+        {
+            _scaleEffect.Scale = Vector2.Divide(_scaleEffect.Scale, DefaultZoomFactor);
+            ZoomUpdated();
+            ImageCanvas.Invalidate();
+        }
+
+        private async void ColorPicker_Click(object sender, RoutedEventArgs e)
+        {
+            var colorPicker = new ColorPickerDialog();
+            await colorPicker.ShowAsync();
+        }
+    }
+}
